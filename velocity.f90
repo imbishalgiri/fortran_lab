e6db7a2bc5d53fd7e2fe36b40b6f716c7da09b3d
bishal giri <giribishal09@gmail.com>
2020-11-05 17:33:12 +0545

    first program in fortran just playing around with variables

---
+++ velocity.f90
@@ -0,0 +1,22 @@
+!---------------------------------------------------------
+!            Pokhara University
+!       Nepal College Of Information Technology
+!       Department of Software Engineering
+!       
+!       Principles of Programming language
+!
+!       Author: Bishal Giri (bishal.171743@ncit.edu.np)
+!------------------------------------------------------------
+! Demonstration of simple program in Fortran that uses 
+! some primitive variables 
+!------------------------------------------------------------
+
+! A simple program to calculate velocity !
+
+program velocity
+implicit none
+      real :: s,t,v 
+      t = 4.0
+      v = s / t
+      print *, "the velocity is ", v
+ end program velocity     
